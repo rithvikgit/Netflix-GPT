51f470f3428bd78fdac4870b0416360bd3e2f58c
rithvikgit <rithvik.r.poojary98@gmail.com>
2024-08-24 21:16:20 +0530

    Bug Fix.

---
+++ src/utils/constants.js
@@ -0,0 +1,16 @@
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const USER_AVATAR =
+  "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp";
+
+export const LOGIN_BACKGROUNG =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/b2c3e95b-b7b5-4bb7-a883-f4bfc7472fb7/19fc1a4c-82db-4481-ad08-3a1dffbb8c39/IN-en-20240805-POP_SIGNUP_TWO_WEEKS-perspective_WEB_24a485f6-1820-42be-9b60-1b066f1eb869_large.jpg";
+
+export const API_OPTIONS = {
+    method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: process.env.TMDB_AUTHORIZATION
+  }
+};
